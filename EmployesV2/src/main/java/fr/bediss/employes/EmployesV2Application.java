cf0e020b44cdf40585b1f77f2c351e3d4a211896
Bédiss NAHDI <nahdibediss@yahoo.fr>
2021-11-21 09:17:18 +0100

    Création du projet Employes V2 sur Eclipse

---
+++ EmployesV2/src/main/java/fr/bediss/employes/EmployesV2Application.java
@@ -0,0 +1,13 @@
+package fr.bediss.employes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployesV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployesV2Application.class, args);
+	}
+
+}
---
+++ EmployesV2/src/test/java/fr/bediss/employes/EmployesV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bediss.employes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployesV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
